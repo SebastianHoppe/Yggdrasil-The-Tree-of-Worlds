880845ccdb8fc9fefdb60c3888947292ea489347
SebastianHoppe <sebastian.hoppe@mail.de>
2016-02-11 15:21:54 +0100

    Added: GameObject class, InputManager class, new Tileset

---
+++ Yggdrasil-The-Tree-of-Worlds/GameObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Yggdrasil_The_Tree_of_Worlds
+{
+   abstract class GameObject
+    {
+        public string Name = "GameObject";
+
+        public virtual void Update(GameTime gameTime)
+        {
+
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
---
+++ Yggdrasil-The-Tree-of-Worlds/InputManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+namespace Yggdrasil_The_Tree_of_Worlds
+{
+   static class InputManager
+    {
+        public delegate void InputEventHandler(Keys key);
+        public static event InputEventHandler OnKeyPressed, OnKeyReleased, OnKeyDown;
+
+        private static Keys[] lastFrameKeys = new Keys[0];
+        private static Keys[] currentKeys = new Keys[0];
+
+        public static void Update()
+        {
+            lastFrameKeys = currentKeys;
+
+            KeyboardState keyState = Keyboard.GetState();
+            currentKeys = keyState.GetPressedKeys();
+
+            foreach (var key in currentKeys)
+            {
+                if (OnKeyDown != null)
+                    OnKeyDown(key);
+
+                bool IsAlreadyPressed = false;
+                foreach (var lastKey in lastFrameKeys)
+                    if (key == lastKey)
+                        IsAlreadyPressed = true;
+
+                if (!IsAlreadyPressed)
+                    if (OnKeyPressed != null)
+                        OnKeyPressed(key);
+            }
+
+            foreach (var key in lastFrameKeys)
+            {
+                bool isReleased = keyState.IsKeyUp(key);
+                if (isReleased)
+                    if (OnKeyReleased != null)
+                        OnKeyReleased(key);
+            }
+        }
+
+
+    }
+}
