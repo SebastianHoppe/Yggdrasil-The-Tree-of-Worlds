b2209f6c271689093f79e34133e5ccd18e907690
SebastianHoppe <sebastian.hoppe@mail.de>
2016-02-11 14:05:44 +0100

    Added: SceneManager class, Sprite Enemy, Sprite BossEnemy, Sprite Player, Tiles

--- Yggdrasil-The-Tree-of-Worlds/LevelScene.cs
+++ Yggdrasil-The-Tree-of-Worlds/LevelScene.cs
@@ -10,6 +10,16 @@ namespace Yggdrasil_The_Tree_of_Worlds
 {
     class LevelScene : Scene
     {
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+        }
 
+        public override void LoadContent()
+        {
+        }
+
+        public override void Update(GameTime gametime)
+        {
+        }
     }
 }
---
+++ Yggdrasil-The-Tree-of-Worlds/SceneManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Yggdrasil_The_Tree_of_Worlds
+{
+   static class SceneManager
+    {
+        public static Scene CurrentScene { get; private set; }
+
+        public static void LoadScene<T>() where T : Scene, new()
+        {
+            CurrentScene = new T();
+            CurrentScene.LoadContent();
+        }
+
+        public static void UpdateScene(GameTime gameTime)
+        {
+            CurrentScene.Update(gameTime);
+        }
+
+        public static void DrawScene(SpriteBatch spriteBatch)
+        {
+            CurrentScene.Draw(spriteBatch);
+        }
+    }
+}
